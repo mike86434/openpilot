50314474ad95c02cc6dc2407aa53fcae1a16bf1c
mike8643 <nguyen95465@yahoo.com>
2026-08-03 11:20:23 -0400

    stock_cp

--- opendbc_repo/opendbc/car/honda/interface.py
+++ opendbc_repo/opendbc/car/honda/interface.py
@@ -311,9 +311,9 @@ class CarInterface(CarInterfaceBase):
             stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 1663], [0, 1663]]
             _low_max = 25. * CV.MPH_TO_MS
             _bp = [0., _low_max - 1e-3, _low_max, 50. * CV.MPH_TO_MS]
-            ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kpV = [_bp, [0.018, 0.024, 0.048, 0.060]]
-            ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kiV = [_bp, [0.006, 0.008, 0.016, 0.020]]
-            ret.lateralTuning.pid.kf = 3.6e-6
+            stock_cp.lateralTuning.pid.kpBP, stock_cp.lateralTuning.pid.kpV = [_bp, [0.018, 0.024, 0.048, 0.060]]
+            stock_cp.lateralTuning.pid.kiBP, stock_cp.lateralTuning.pid.kiV = [_bp, [0.006, 0.008, 0.016, 0.020]]
+            stock_cp.lateralTuning.pid.kf = 3.6e-6
       else:
         stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 2560], [0, 2560]]
         stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.8], [0.24]]
